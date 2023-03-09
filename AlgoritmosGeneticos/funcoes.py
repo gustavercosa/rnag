83b0eaa3b863c96a6c640748973719146c244c3c
Gustavo Duarte Verçosa <gustavo220062@ilum.cnpem.br>
2023-03-09 20:15:05 -0300

    introdução

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,10 @@
- 
+def funcao_objetiva_cb(individuo):
+    """Computa a função objetiva no problema das caixas binárias.
+
+    Argumentos:
+        individuos: lista contendo os genes das caixas binárias.
+
+    Retorna:
+        Um valor representando a soma dos genes do indivíduo.
+    """
+    return sum(individuo) 
